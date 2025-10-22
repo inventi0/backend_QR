f2f473013dc50a23caa4e26262c2eabe31280e28
oliceglad <vycheslavofficial@gmail.com>
2025-10-23 02:33:26 +0400

    added new port

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.routes.user:app",
         host="0.0.0.0",
-        port=8080,
+        port=80,
         reload=True,
         workers=1
     )
